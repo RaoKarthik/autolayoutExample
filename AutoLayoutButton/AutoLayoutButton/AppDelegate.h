b5a90e05bfbe61427676e3c2a1a65007460938c5
Karthik Rao <karthik@techjini.com>
2015-02-28 12:39:29 +0530

    Initial commit

---
+++ AutoLayoutButton/AutoLayoutButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutButton
+//
+//  Created by Karthik Rao on 28/02/15.
+//  Copyright (c) 2015 Karthik Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutButton/AutoLayoutButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutButton
+//
+//  Created by Karthik Rao on 28/02/15.
+//  Copyright (c) 2015 Karthik Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
